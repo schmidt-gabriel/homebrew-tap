d074712209d788482609de7b9c964d49bdc26985
Gabriel Schmidt <schmidt-gabriel@users.noreply.github.com>
2026-03-13 14:02:20 -0300

    Update SHA256 checksums for flowt formula

--- Formula/flowt.rb
+++ Formula/flowt.rb
@@ -6,24 +6,24 @@ class Flowt < Formula
   on_macos do
     on_arm do
       url "https://github.com/schmidt-gabriel/flowt/releases/download/v#{version}/flowt-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_AARCH64_SHA256"
+      sha256 "sha256:928c42fad1bf7963d8b1df58753aa5edff267675bb0776e4319798c7b2cae397"
     end
 
     on_intel do
       url "https://github.com/schmidt-gabriel/flowt/releases/download/v#{version}/flowt-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_X86_64_SHA256"
+      sha256 "sha256:3f11d2149e26c5f8a58d5f1b9060c01be7658c18ea0b41c8854ffa595c35c1b2"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/schmidt-gabriel/flowt/releases/download/v#{version}/flowt-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_AARCH64_SHA256"
+      sha256 "sha256:a51cdf5f7ec87a988416d07e52b7b69c848b6b33bdf19397bfd56a7656db415e"
     end
 
     on_intel do
       url "https://github.com/schmidt-gabriel/flowt/releases/download/v#{version}/flowt-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_X86_64_SHA256"
+      sha256 "sha256:d58e49f7e678cd04459b528438680935cdf5ea0f5d686230bd439cf7ba8f36ff"
     end
   end
 
