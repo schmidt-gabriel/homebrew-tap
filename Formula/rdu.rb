53d79de278623a2bbb2ea5e67beb38e24d0f8f68
Gabriel Schmidt <schmidt-gabriel@users.noreply.github.com>
2026-03-19 17:58:37 -0300

    Update Rdu formula version to 0.1.1 and SHA256 checksums

--- Formula/rdu.rb
+++ Formula/rdu.rb
@@ -1,29 +1,29 @@
 class Rdu < Formula
   desc "A terminal disk usage analyzer written in Rust"
   homepage "https://github.com/schmidt-gabriel/rdu"
-  version "0.1.0"
+  version "0.1.1"
 
   on_macos do
     on_arm do
       url "https://github.com/schmidt-gabriel/rdu/releases/download/v#{version}/rdu-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "9ea1c7eae237a248074dc760dec59f7d1cb61b61e142b8d6f9add79848326fc7"
+      sha256 "ea8a428efe9ac2088cde2a69128299ca4f6be5159e97b957268c561601d86824"
     end
 
     on_intel do
       url "https://github.com/schmidt-gabriel/rdu/releases/download/v#{version}/rdu-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "6c2e84e25714f25bfa3d7b9857861fdf54949df2b0562b85928286f6f68e4dec"
+      sha256 "3fb72991660ec3a90fc59b361d4b226c3e5e681801adff4731f1bab97198aede"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/schmidt-gabriel/rdu/releases/download/v#{version}/rdu-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "2794c6179ea12c1c421a361dfdc07bc84be37ecadf2227ae882abed6cd85531b"
+      sha256 "6d744ed549e01dd6bd0123aded4094970146900df0a6299108ec04dcad7dbcde"
     end
 
     on_intel do
       url "https://github.com/schmidt-gabriel/rdu/releases/download/v#{version}/rdu-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "47b24639d81791a87935673f226b5e447a580fa408d68206ded91198edcbf784"
+      sha256 "9affae2f64b2591cef2caa1b659db811c378f2196dba3399d2389c0149be1993"
     end
   end
 
