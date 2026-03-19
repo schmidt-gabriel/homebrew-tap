42830920d3d0afca5a793befba4e0abea18a0e4f
Gabriel Schmidt <schmidt-gabriel@users.noreply.github.com>
2026-03-19 15:56:30 -0300

    Update SHA256 checksums for Rdu formula on macOS and Linux

--- Formula/rdu.rb
+++ Formula/rdu.rb
@@ -6,24 +6,24 @@ class Rdu < Formula
   on_macos do
     on_arm do
       url "https://github.com/schmidt-gabriel/rdu/releases/download/v#{version}/rdu-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "8dce84e02dbc5b71452f8fa31e63e33085475523c58e2e7723b766b662766519"
+      sha256 "9ea1c7eae237a248074dc760dec59f7d1cb61b61e142b8d6f9add79848326fc7"
     end
 
     on_intel do
       url "https://github.com/schmidt-gabriel/rdu/releases/download/v#{version}/rdu-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "e1e152d9d4fa3db68ffb394c23ee9f5c0493b518802151f1841aa114d61b0b63"
+      sha256 "6c2e84e25714f25bfa3d7b9857861fdf54949df2b0562b85928286f6f68e4dec"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/schmidt-gabriel/rdu/releases/download/v#{version}/rdu-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "6a40a566bbe06097f7fc3d6c937e03a6b42ff9542fc95295fb201a148aaa86ce"
+      sha256 "2794c6179ea12c1c421a361dfdc07bc84be37ecadf2227ae882abed6cd85531b"
     end
 
     on_intel do
       url "https://github.com/schmidt-gabriel/rdu/releases/download/v#{version}/rdu-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "eaf87356390bf942da2ff16fc927bd61714c6bb217ca6c7186807fdc50f9afb3"
+      sha256 "47b24639d81791a87935673f226b5e447a580fa408d68206ded91198edcbf784"
     end
   end
 
